191e1d40566ee42c02ab60f135f01a5cd56556b5
vishnu <pvishnutech@gmailcom>
2024-06-03 08:35:55 +0530

    test folder with the new updated scripts

---
+++ test folder/CommandIfExistsOrNot.sh
@@ -0,0 +1,4 @@
+if ! command_exists "wget"; then
+	echo "Error wget command is not found" >&2
+	exit 1
+fi
---
+++ test folder/array.sh
@@ -0,0 +1,6 @@
+
+
+names=("rohini" "sai" "prasad" "rajaji")
+for name in "${names[@]}"; do
+    echo "name is $name"
+done
